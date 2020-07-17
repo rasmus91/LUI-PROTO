44cbfa96dfecc07b47d75fa2a4bf40940600a30b
Rasmus Lindegaard <rasmusnielsen91@gmail.com>
2020-07-17 22:45:45 +0200

    Navigation menu finished

---
+++ LUI.js
@@ -0,0 +1,14 @@
+
+
+
+//enables activation/deactivation of the navigation menu by pressing the burgermenu (before area of the lui-nav-bar-menu)
+document.getElementsByClassName('lui-nav-bar-menu')[0].addEventListener('click', function(e){
+    this.classList.contains('active') ?
+        this.classList.remove('active') :
+        this.classList.add('active');
+});
+
+//Prevents activation/deactivation of the nav menu by clicking the navigation area
+document.getElementsByClassName('lui-nav-bar-link-area')[0].addEventListener('click', function(e){
+    e.stopPropagation();
+});
